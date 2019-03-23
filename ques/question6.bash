480ca1803dc1f85017a343bb6f4647ac25791194
hugues <hugues.guilleus@ens.uvsq.fr>
2019-03-23 16:18:30 +0100

    Q6 bash: ajout commentaires et 'résultat = ' + Q6+: corrections tests

--- ques/question6.bash
+++ ques/question6.bash
@@ -1,17 +1,20 @@
 #!/bin/bash
 # HENROTTE Hugo CPI <henrotte.hugo@gmail.com>
+
+# on test si il n'y a aucun argument
 if [[ $# == 0 ]]
 then
 	echo "** pas d'argument **"
 	exit 1
 fi
 
+# on calcule chaque expression
 res=$1
-
 for i in `seq 2 2 $#`
 do
 	after=$(( i + 1 ))
 	res=`ques/question3.bash $res ${!i} ${!after}`
 done
 
-echo $res
+# on affiche le résultat
+echo "résultat = $res"
