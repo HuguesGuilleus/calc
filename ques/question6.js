2b582b0c7b5be20c695f8c4772dd372d11e5748c
hugues <hugues.guilleus@ens.uvsq.fr>
2019-04-07 09:43:09 +0200

    Q6 Js: mise en module

--- ques/question6.js
+++ ques/question6.js
@@ -1,20 +1,26 @@
 #!/usr/bin/nodejs
 // GUILLEUS Hugues CPI2 <hugues.guilleus@ens.uvsq.fr>
 
-var cp = require('child_process');
+const q3 = require("./ques/question3.js");
 
-try {
-	var res = process.argv[2];
-	for (let i = 3; i < process.argv.length; i+=2) {
-		// on clclue grâce à la question 3,
-		// on récupère stdout que l'on tranforme en chaîne de caractères
-		// et on enlève la dernière ligne
-		res = cp.execSync("ques/question3.js "+res+" "+process.argv[i]+" "+process.argv[i+1])
-			.toString().replace("\n","");
+
+if (require.main === module) {
+	console.log("résultat =", calcExpr( process.argv.slice(2) ) );
+} else {
+	module.exports = {
+		calcExpr:calcExpr,
 	}
-} catch (err) {
-	console.error(err);
-	process.exit(1);
 }
 
-console.log("résultat =", res);
+
+/**
+	@argument args {[]String}
+	@ret res {Number} résultat du calcule (pas de priorité des opérateurs)
+*/
+function calcExpr(args) {
+	var res = args[0];
+	for (let i=1; i<args.length; i+=2) {
+		res = q3.calc(res, args[i], args[i+1]);
+	}
+	return res;
+}
