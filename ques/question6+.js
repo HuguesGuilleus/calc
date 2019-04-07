aa958217f91b097c3a3b52a80dbdfaf393104fa9
hugues <hugues.guilleus@ens.uvsq.fr>
2019-04-07 10:23:03 +0200

    Q6 Js: création module

--- ques/question6+.js
+++ ques/question6+.js
@@ -1,59 +1,59 @@
 #!/usr/bin/nodejs
 // GUILLEUS Hugues CPI2 <hugues.guilleus@ens.uvsq.fr>
 
-var cp = require('child_process');
+const q2 = require("./ques/question2.js");
+const q3 = require("./ques/question3.js");
 
-try {
-	// on copie les arguments utiles passés au programme
-	args=process.argv.slice(2);
-
-	// on regarde si le premier argument est une somme ou une soustraction
-	switch (cp.execSync("ques/question2.js "+args[0]).toString().replace("\n","")) {
-		case "somme":
-			args.shift();
-			break;
-		case "sous":
-			args.splice(0,1, "-1", "x");
-			break;
-	}
-
-	// on sépare les opérations mult et div que l'on calcule
-	groupeop=[]; // groupe d'opération somme et sous
-	g = [args[0]]; // groupe d'opération mult et div
-	for (let i = 1; i < args.length; i+=2) {
-		switch (cp.execSync("ques/question2.js "+args[i]).toString().replace("\n","")) {
-			case "mult":
-			case "div":
-				g.push(args[i], args[i+1])
-				break;
-			case "somme":
-			case "sous":
-				groupeop.push( calcule(g), args[i]);
-				g = [args[i+1]];
-				break;
-		}
-	}
-
-	// on fait le calcule du dernier argument
-	groupeop.push( calcule(g));
-
-	// on calule tout et on affiche
-	console.log( calcule(groupeop) );
-} catch (err) {
-	console.error(err);
-	process.exit(1)
+if (require.main === module) {
+	console.log( calcExprPrio(process.argv.slice(2)) );
+} else {
+	module.exports = {
+		calcExpr:calcExprPrio,
+	};
 }
 
 
-// fonction qui calcule le tableau d'argument: ['5', '+', '5', '/', "4"]
-function calcule(tab) {
-	var res = tab[0];
-	for (let i = 1; i < tab.length; i+=2) {
-		// on calcule grâce à la question 3,
-		// on récupère stdout que l'on tranforme en chaîne de caractères
-		// et on enlève la dernière ligne
-		res = cp.execSync("ques/question3.js "+res+" "+tab[i]+" "+tab[i+1])
-			.toString().replace("\n","");
+/**
+	@argument args {[]String}
+	@ret res {Number} résultat du calcule (priorité des opérateurs)
+*/
+function calcExprPrio(arg){
+	// Si le premier élément est un plus ou un moins
+	switch (q2.type(arg[0])) {
+		case "somme":
+			arg.shift();
+			break;
+		case "sous":
+			arg.splice(0,1,"-1","x");
+			break;
 	}
-	return res;
+
+	var termes = []; // somme et soustraction
+	var facteurs = [arg[0]]; // multiplication et division
+	for(let i=1; i<arg.length; i+=2){
+		switch (q2.type(arg[i])) {
+			case "mult":
+			case "div":
+				facteurs.push(arg[i], arg[i+1]);
+				break;
+			default:
+				termes.push( calcExprSimple(facteurs), arg[i] )
+				facteurs = [arg[i+1]];
+		}
+	}
+	termes.push(calcExprSimple(facteurs));
+	return calcExprSimple(termes);
+}
+
+
+/**
+	@argument args {[]String}
+	@ret res {Number} résultat du calcule (pas de priorité des opérateurs)
+*/
+function calcExprSimple(arg) {
+	resultat = arg[0];
+	for(let i=1; i<arg.length; i+=2){
+		resultat = q3.calc(resultat,arg[i],arg[i+1]);
+	}
+	return resultat;
 }
